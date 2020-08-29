22d88083b190137e6757ded77aa398c452d2515c
Sathvik777 <katam.sathvik@gmail.com>
2020-08-29 10:29:35 +0200

    fix: Match paths for hemnet

--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,18 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 let changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', function(data) {
+  // TODO: Add estimator button
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
 
 changeColor.onclick = function(element) {
+  // TODO: Get the feature value we need
   let color = element.target.value;
+  // TODO: 1. Make a query request to API
+  // 2. Get results and display it
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
